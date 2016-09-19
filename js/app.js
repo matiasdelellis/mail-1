9b1dc8978f6bd9f3eb7b337597490ff1f0d92798
Christoph Wurst <christoph@winzerhof-wurst.at>
2016-09-19 17:10:54 +0200

    ItemView -> View; LayoutView -> View

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ define(function(require) {
 	Marionette.TemplateCache.prototype.compileTemplate = function(rawTemplate) {
 		return Handlebars.compile(rawTemplate);
 	};
-	Marionette.ItemView.prototype.modelEvents = {change: 'render'};
+	Marionette.View.prototype.modelEvents = {change: 'render'};
 	Marionette.CompositeView.prototype.modelEvents = {change: 'render'};
 
 	var Mail = Marionette.Application.extend({
