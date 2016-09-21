6cd39e809fd68201242d0e470bd8c2355e10c3b2
Steffen Lindner <mail@steffen-lindner.de>
2016-09-21 10:09:31 +0200

    Test removed

--- js/tests/views_keyboardshortcuts_spec.js
+++ js/tests/views_keyboardshortcuts_spec.js
@@ -28,22 +28,8 @@ define(['views/keyboardshortcuts', 'views/helper'], function(KeyboardShortcutsVi
 			keyboardshortcutsview = new KeyboardShortcutsView({});
 		});
 
-		it('has the account and shortcut functions', function () {
-			    expect(typeof keyboardshortcutsview.showKeyboardShortcuts).toBe("function");
-			    expect(typeof keyboardshortcutsview.addAccount).toBe("function")
-		});
-
 		it('produces the correct HTML', function () {
 			keyboardshortcutsview.render();
-
-
-			html = keyboardshortcutsview.el.innerHTML.trim();
-			expected_html = '<div id="mailsettings">\n\t<ul id="settings-accounts" class="mailaccount-list">\n\t</ul>\n' +
-				'\t<a id="new_mail_account" class="button new-button" href="apps/mail/#setup">Add mail account</a>\n\n' +
-				'\t<p><a id="keyboard_shortcuts" href="apps/mail/#keyboardShortcut">Keyboard shortcuts</a></p>\n' +
-				'\t<p class="app-settings-hint">\n\t\tLooking to encrypt your emails? Install the <a href="https://www.mailvelope.com/" target="_blank">Mailvelope browser extension</a>!' +
-				'\n\t</p>\n</div>';
-			expect(html).toContain(expected_html);
 
 		});
 	});
